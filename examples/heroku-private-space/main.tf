c0cd9bdd5683879c8d0b41cd9c93e9597c56d1a9
Jack Ziesing <jack.ziesing@jziesing-wsm4.internal.salesforce.com>
2021-01-27 17:20:38 -0800

    updated heroku ps example

--- examples/heroku-private-space/main.tf
+++ examples/heroku-private-space/main.tf
@@ -1,45 +1,53 @@
-provider "google" {
-  version = "~> 2.1"
-  region  = "${var.google_region}"
+terraform {
+  required_providers {
+    heroku = {
+      source  = "heroku/heroku"
+      version = "3.2.0"
+    }
+    google = {
+      source  = "google"
+      version = "~> 2.0"
+    }
+  }
 }
 
-provider "heroku" {
-  version = "~> 1.8"
+provider "google" {
+  region = var.google_region
 }
 
 module "heroku_vpn_gcp" {
-  source = "../../"
+  source = "github.com/heroku-examples/terraform-heroku-vpn-gcp"
 
   // Google Cloud Platform config
-  google_region                          = "${var.google_region}"
-  google_network                         = "${var.google_network}"
-  google_network_auto_create_subnetworks = "${var.google_network_auto_create_subnetworks}"
-  google_subnetwork                      = "${var.google_subnetwork}"
-  google_subnetwork_cidr_block           = "${var.google_subnetwork_cidr_block}"
-  google_subnetwork_private_ip_access    = "${var.google_subnetwork_private_ip_access}"
+  google_region                          = var.google_region
+  google_network                         = var.google_network
+  google_network_auto_create_subnetworks = var.google_network_auto_create_subnetworks
+  google_subnetwork                      = var.google_subnetwork
+  google_subnetwork_cidr_block           = var.google_subnetwork_cidr_block
+  google_subnetwork_private_ip_access    = var.google_subnetwork_private_ip_access
 
   // Heroku Private Space config
-  vpn_remote_cidr_block       = "${heroku_space_vpn_connection.google.space_cidr_block}"
-  vpn_ike_version             = "${heroku_space_vpn_connection.google.ike_version}"
-  vpn_tunnel_ip_0             = "${heroku_space_vpn_connection.google.tunnels.0.ip}"
-  vpn_tunnel_pre_shared_key_0 = "${heroku_space_vpn_connection.google.tunnels.0.pre_shared_key}"
-  vpn_tunnel_ip_1             = "${heroku_space_vpn_connection.google.tunnels.1.ip}"
-  vpn_tunnel_pre_shared_key_1 = "${heroku_space_vpn_connection.google.tunnels.1.pre_shared_key}"
+  vpn_remote_cidr_block       = heroku_space_vpn_connection.google.space_cidr_block
+  vpn_ike_version             = heroku_space_vpn_connection.google.ike_version
+  vpn_tunnel_ip_0             = heroku_space_vpn_connection.google.tunnels.0.ip
+  vpn_tunnel_pre_shared_key_0 = heroku_space_vpn_connection.google.tunnels.0.pre_shared_key
+  vpn_tunnel_ip_1             = heroku_space_vpn_connection.google.tunnels.1.ip
+  vpn_tunnel_pre_shared_key_1 = heroku_space_vpn_connection.google.tunnels.1.pre_shared_key
 
   providers = {
-    google = "google"
+    google = google
   }
 }
 
 resource "heroku_space" "default" {
-  name         = "${var.heroku_private_space}"
-  organization = "${var.heroku_enterprise_team}"
-  region       = "${var.heroku_private_space_region}"
+  name         = var.heroku_private_space
+  organization = var.heroku_enterprise_team
+  region       = var.heroku_private_space_region
 }
 
 resource "heroku_space_vpn_connection" "google" {
-  name           = "${var.heroku_vpn}"
-  space          = "${heroku_space.default.id}"
-  public_ip      = "${module.heroku_vpn_gcp.google_vpn_ip}"
-  routable_cidrs = ["${module.heroku_vpn_gcp.google_cidr_block}"]
+  name           = var.heroku_vpn
+  space          = heroku_space.default.id
+  public_ip      = module.heroku_vpn_gcp.google_vpn_ip
+  routable_cidrs = [module.heroku_vpn_gcp.google_cidr_block]
 }
